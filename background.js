6706ba3d2e4774a1a296df53f5d79e61195ab699
Adel <93949685+Adelxoxo@users.noreply.github.com>
2024-10-01 05:42:02 +0200

    live instance bugfix

--- background.js
+++ background.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
         'quake': [
             { img: 'https://images5.alphacoders.com/127/1276278.png' },
             { img: 'https://images5.alphacoders.com/927/927639.jpg' },
-            { img: '../Adelv2/media/images/backgrounds/background1.jpg' },
-            { img: '../Adelv2/media/images/backgrounds/background3.jpg' }
+            { img: '../media/images/backgrounds/background1.jpg' },
+            { img: '../media/images/backgrounds/background3.jpg' }
         ],
         'tactical': [
-            { img: '../Adelv2/media/images/backgrounds/background5.jpg' },
-            { img: '../Adelv2/media/images/backgrounds/background6.jpg' },
-            { img: '../Adelv2/media/images/backgrounds/background7.jpg' },
-            { img: '../Adelv2/media/images/backgrounds/background8.jpg' }
+            { img: '../media/images/backgrounds/background5.jpg' },
+            { img: '../media/images/backgrounds/background6.jpg' },
+            { img: '../media/images/backgrounds/background7.jpg' },
+            { img: '../media/images/backgrounds/background8.jpg' }
         ],
         'adel': [
-            { img: '../Adelv2/media/images/backgrounds/background9.png' }
+            { img: '../media/images/backgrounds/background9.png' }
         ],
     };
 
